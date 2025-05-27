f0253c8b05207f7c6132cf2f040229edde94da1b
sudarsancse <sudarsansarkarcst@gmail.com>
2025-05-27 16:48:03 +0530

    update user route and image uplod route functions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -93,6 +93,26 @@ export const getUserProfile = async (req, res) => {
 //? Update user profile
 export const updateUserProfile = async (req, res) => {
   try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(201).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      role: updatedUser.role,
+      token: generateToken(updatedUser._id),
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,6 +6,7 @@ import {
   updateUserProfile,
 } from "../controllers/authController.js";
 import { protect } from "../middlewares/authMiddleware.js";
+import uplode from "../middlewares/uplodeMiddleware.js";
 
 const routes = express.Router();
 
@@ -13,5 +14,17 @@ routes.post("/register", registerUser);
 routes.post("/login", loginUser);
 routes.get("/profile", protect, getUserProfile);
 routes.put("/profile", protect, updateUserProfile);
+
+//
+routes.post("/upload-image", uplode.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(404).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+
+  res.status(200).json({ imageUrl });
+});
 
 export default routes;
