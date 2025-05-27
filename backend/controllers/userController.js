58edc80317100f837a54f8bcd672c75d6691d745
sudarsancse <sudarsansarkarcst@gmail.com>
2025-05-27 17:52:04 +0530

    user find by id (USer management Routes)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,6 +44,11 @@ export const getUsers = async (req, res) => {
 //@access           Private (admin olny)
 export const getUserById = async (req, res) => {
   try {
+    const user = await User.findById(req.params.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
