5419b640db760b4df8f30f2a7dd2bbc6783d84b7
sudarsancse <sudarsansarkarcst@gmail.com>
2025-05-27 23:13:05 +0530

    get task by id

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -103,6 +103,14 @@ export const getTasks = async (req, res) => {
 //@access           Private
 export const getTaskById = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id).populate(
+      "assignedTo",
+      "name email profileImageUrl"
+    );
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    res.json(task);
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
