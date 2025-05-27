80ee3dbd62cc02dce83893a5a95d25c5a745dc4a
sudarsancse <sudarsansarkarcst@gmail.com>
2025-05-27 23:41:45 +0530

    delete task by id

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -159,6 +159,12 @@ export const createTask = async (req, res) => {
 //@access           Private (admin olny)
 export const deleteTask = async (req, res) => {
   try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    await task.deleteOne();
+    res.status(200).json({ message: "Task deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error", error: error.message });
   }
